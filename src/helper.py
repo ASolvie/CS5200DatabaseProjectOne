3d25a368512993078a9ec7abc01012f1a989df1d
ASolvie <austen@solvie.com>
2023-10-31 21:05:07 -0500

    another helper

--- src/helper.py
+++ src/helper.py
@@ -80,3 +80,12 @@ def create_database_from_folders(database_name: str, folder_name: str):
         print(que)
         cursor.execute(que)
     connection.commit()
+
+def create_select_columns_from_old_table(new_table_name, old_table_name, columns):
+    query = f'CREATE TABLE {new_table_name} AS (SELECT'
+    for column in columns:
+        query = f'{query} column'
+        if(columns[-1] != column):
+            query = f'{query},'
+    query = f'{query} FROM {old_table_name})'
+    return query
