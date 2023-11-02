2fff3c8269d921a850df5835c39155d0f01a3846
xmr9xb <143531912+xmr9xb@users.noreply.github.com>
2023-11-02 18:10:16 -0500

    temp update 4NF

--- 4NF_code.py
+++ 4NF_code.py
@@ -3,7 +3,7 @@ import csv
 import helper
 
 # Helper function to split a table based on MVDs
-def split_table(table_name, fd, mvd):
+def split_table(table_name, mvd: list[helper.Relaion]):
     cursor.execute(f"SELECT * FROM {table_name}")
     rows = cursor.fetchall()
 
@@ -12,20 +12,18 @@ def split_table(table_name, fd, mvd):
     columns = [col[1] for col in cursor.fetchall()]
 
     # Create new tables to store the split data
-    # TODO: fix this by fixing how MVDs are handled
-    for X, Y in mvd:  # fix how MVDs are handled here
-        new_table_name = f"{table_name}_{X}_{Y}"
-        cursor.execute(f"CREATE TABLE IF NOT EXISTS {new_table_name} ({X} TEXT, {Y} TEXT)")
+    for dep in mvd:  
+        new_table_name = f"{table_name}_{dep.x}_{dep.y}"
+        cursor.execute(f"CREATE TABLE IF NOT EXISTS {new_table_name} ({dep.x} TEXT, {dep.y} TEXT)")
 
     # Populate the new tables
-    # TODO: fix this by fixing how MVDs are handled
     for row in rows:
-        for X, Y in mvd:
-            x_value = row[columns.index(X)]
-            y_values = row[columns.index(Y)].split(',')  # fix how MVDs are handled here
+        for dep in mvd:
+            x_value = row[columns.index(dep.x)]
+            y_values = row[columns.index(dep.y)]
 
             for y_value in y_values:
-                cursor.execute(f"INSERT INTO {table_name}_{X}_{Y} ({X}, {Y}) VALUES (?, ?)", (x_value, y_value))
+                cursor.execute(f"INSERT INTO {table_name}_{mvd.x}_{mvd.y} ({mvd.x}, {mvd.y}) VALUES (?, ?)", (x_value, y_value))
 
     # Remove original table
     cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
@@ -35,27 +33,19 @@ def convertTo4NF(db_name):
     # Connect to your .db file
     conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
-    
-    # Retrieve a list of all table names
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    tables = cursor.fetchall()
-    
+
     # Initialize a dictionary to store functional dependencies and multivalued dependencies
     functional_dependencies = helper.read_in_relations("data/relations")
     multivalued_dependencies = helper.readInMVDs("data/Multivalued")
+    
+    # Create a list of all table names needed
+    tables = []
+    for a in multivalued_dependencies:
+        tables.append(helper.find_table_with_columns(db_name, [a.x, a.y]))
 
-    # Iterate over the tables and process each one
-    for table_name in tables:
-        table_name = table_name[0]
-    
-        # Check if the table has multivalued dependencies
-        if table_name in multivalued_dependencies:
-            mvd = multivalued_dependencies[table_name]
-    
-            # Check if the table has functional dependencies
-            if table_name in functional_dependencies:
-                fd = functional_dependencies[table_name]
-                split_table(table_name, fd, mvd)
+    # Check if the table has multivalued dependencies
+    for table_name, mvd in zip(tables, multivalued_dependencies):
+        split_table(table_name, mvd)
     
     # Commit the changes and close the connection
     conn.commit()
