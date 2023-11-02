410a94e8dbbe183f7099b741aa673725c8f9746b
WesleyTyree <wdttck@umsystem.edu>
2023-11-01 22:14:55 -0500

    3NF fully working

--- src/3NF_Code.py
+++ src/3NF_Code.py
@@ -3,6 +3,7 @@ from tkinter.tix import COLUMN
 import helper
 import sqlite3
 import os
+from num2words import num2words
 
 try:
     os.remove('ddo.db') # removes old ddo.db file
@@ -27,38 +28,30 @@ def three_n_f(relations: list[helper.Relation], transitive_deps: list[helper.Rel
     return relations, transitive_deps
 
 helper.create_database_from_folders("ddo", "1NF")
-relations = helper.readInRelations("data/relations")
-primary_keys = helper.findPrimaryKeys(relations)
+relations = helper.read_in_relations("data/relations")
+primary_keys = helper.find_primary_keys(relations)
 
 transitive_deps = find_transitive_dependencies(relations)
-#relations, transitive_deps = three_n_f(relations, transitive_deps)
 
 print("Revised Functional Dependencies:", relations)
 print("transitive dependency", transitive_deps)
 print("Updated Primary Keys:", primary_keys)
 
 # create primary key table
-primary_keys_query = helper.constructCreateTableQuery(1, primary_keys, [])
-newTableNames = ['one','two','three','four']
+primary_keys_query = helper.construct_create_table_query(1, primary_keys, [])
+newTableNames = [num2words(i) for i in range(1, 11)]#['one','two','three','four'] use this list if num2words doesn't work
 
 # create tables from primary key relationss
-name = []
-n = 0
-for derp in primary_keys: 
-    name.append(derp)#[derp] + [value.y for value in relations if value.x == derp])
 queries = []
-#for i in range (len(primary_keys)):
-queries.append(helper.create_select_columns_from_old_table(f'{newTableNames[0]}', 'A', name))
-
-n = n + 1
+n = 0
 name = []
 for derp in primary_keys: 
     name.append([derp] + [value.y for value in relations if value.x == derp])
-#queries = []
+
 for i in range (len(primary_keys)):
         queries.append(helper.create_select_columns_from_old_table(f'{newTableNames[n]}', 'A', name[i]))
         n = n + 1
-#n = i + 1 #to make sure we stay on the same count of new table names
+n = i + 1 #to make sure we stay on the same count of new table names
 
 # create tables from non primary key relations
 name = []
@@ -68,7 +61,7 @@ for i in range (len(transitive_deps)):
         queries.append(helper.create_select_columns_from_old_table(f'{newTableNames[n]}', 'A', name[i]))
         n = n + 1
 
-queries.append(helper.deleteTable('A'))
+queries.append(helper.delete_table_query('A'))
 connection = sqlite3.connect("ddo.db")
 cursor = connection.cursor()
 for que in queries:
