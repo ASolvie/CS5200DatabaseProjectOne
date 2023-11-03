d68dc1b96935dc7ba854f15238f0b244b0110c03
ASolvie <austen@solvie.com>
2023-11-03 11:15:40 -0500

    fixed 5NF code

--- src/FiveNF_Code.py
+++ src/FiveNF_Code.py
@@ -5,16 +5,13 @@ def fifth_normal_form(database_name, primary_keys: list[str]):
     cursor = connection.cursor()
     combinations = []
     queries = []
-    i = 0
-    for p in range(len(primary_keys)):
-            for k in range(p+1, len(primary_keys)):
-                combinations.append((primary_keys[p], primary_keys[k]))
+    prime_keys = [prime.strip() for prime in primary_keys if ',' not in prime]
+    for p in range(len(prime_keys)):
+            for k in range(p+1, len(prime_keys)):
+                combinations.append((prime_keys[p], prime_keys[k]))
     for c in combinations:
-        #queries.append(helper.create_table_query(f'FiveNF{c[0].strip()}_{c[1].strip()}', [c[0], c[1]]))
-        #helper.create_table_query(f'FiveNF{i}', [c[0], c[1]])
-        
-        queries.append(helper.create_table_query(f'FiveNF{i}', [c[0], c[1]]))
-        i += 1
+        print(c)
+        queries.append(helper.create_table_query(f'FiveNF{c[0]}_{c[1]}', [c[0], c[1]]))
     [cursor.execute(query) for query in queries]
     connection.commit()
     connection.close()
