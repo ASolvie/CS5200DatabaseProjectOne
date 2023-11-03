d8e9eb016539b09871d0afd3dd12ddf90b924ea8
ASolvie <austen@solvie.com>
2023-11-02 20:26:03 -0500

    generates table

--- src/5NF_Code.py
+++ src/5NF_Code.py
@@ -1,5 +1,8 @@
 import helper
-def fifth_normal_form(primary_keys: list[str]):
+import sqlite3
+def fifth_normal_form(database_name, primary_keys: list[str]):
+    connection = sqlite3.connect(f'{database_name}.db')
+    cursor = connection.cursor()
     combinations = []
     queries = []
     for p in range(len(primary_keys)):
@@ -7,4 +10,6 @@ def fifth_normal_form(primary_keys: list[str]):
                 combinations.append((primary_keys[p], primary_keys[k]))
     for c in combinations:
         queries.append(helper.create_table_query(f'FiveNF{c[0]}_{c[1]}', [c[0], c[1]]))
-    return queries
+    [cursor.execute(query) for query in queries]
+    connection.commit()
+    connection.close()
